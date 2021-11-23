473fb52509fa6239a7eca8c9cb2b85365c456104
M Abu Baker <abu.baker_e@hotmail.com>
2021-11-23 21:30:39 +0500

    Report Test for Linux

---
+++ Web_ForReport_Core/Controllers/TestController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc; 
+using Microsoft.Reporting.NETCore; 
+namespace ReportViewerCore
+{
+    [ApiController]
+    [Route("api/test")]
+    public class TestController : ControllerBase
+    { 
+         
+        [HttpGet] 
+        [Route("{name}")]
+        public IActionResult Get(string name)
+        { 
+            decimal WidgetPrice = 0;
+            decimal GizmoPrice = 0;
+            using LocalReport report = new LocalReport();
+            Report.Load(report, WidgetPrice, GizmoPrice);
+            var BYTE = report.Render("PDF");
+            return File(BYTE,"application/pdf", name+"_.pdf");
+        }
+    }
+}
---
+++ Web_ForReport_Core/Reports/Report.cs
@@ -0,0 +1,28 @@
+﻿
+using Microsoft.Reporting.NETCore;
+using System.Reflection; 
+namespace ReportViewerCore
+{
+	class Report
+	{
+        public static void Load(LocalReport report, decimal widgetPrice, decimal gizmoPrice)
+        {
+            var items = new[] { new ReportItem { Description = "Widget 6000", Price = widgetPrice, Qty = 1 }, new ReportItem { Description = "Gizmo MAX", Price = gizmoPrice, Qty = 25 } };
+
+            
+            //or from the entry point to the application - there is a difference!
+            var test = Assembly.GetExecutingAssembly().GetManifestResourceNames();
+
+
+            var parameters = new[] { new ReportParameter("Title", "Invoice 4/2020") };
+              //var rs = Assembly.GetExecutingAssembly().GetManifestResourceStream("ReportViewerCore.Sample.AspNetCore.Reports.Report.rdlc");
+              var rs = Assembly.
+                GetExecutingAssembly().
+                GetManifestResourceStream("ReportViewerCore.Sample.AspNetCore.Reports.Report.rdlc");
+            report.LoadReportDefinition(rs);
+            report.DataSources.Add(new ReportDataSource("Items", items));
+            report.SetParameters(parameters);
+            //Web_ForReport_Core\Web_ForReport_Core\Reports\Report.rdlc
+        }
+    }
+}
